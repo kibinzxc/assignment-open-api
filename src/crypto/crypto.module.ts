6a58db7d5988424ff2221bdb448ae9752a809b07
Kevin <mamadaddyalexa@gmail.com>
2025-01-11 22:49:09 +0800

    implement READ crypto data from cryptoAPI

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { CryptoService } from './crypto.service';
 import { CryptoController } from './crypto.controller';
 
 @Module({
+  imports: [HttpModule],
+  controllers: [CryptoController],
   providers: [CryptoService],
-  controllers: [CryptoController]
 })
-export class CryptoModule {}
+export class CryptoModule { }
